ebc8fcafe91bc21ceee88d3c6556407076cc32b8
Treefunder Incorporated <ceo@treefunder.com>
2014-10-25 05:52:42 -0400

    Czarblockd Factoring

--- lib/util.py
+++ lib/util.py
@@ -98,7 +98,6 @@ def call_jsonrpc_api(method, params=None, endpoint=None, auth=None, abort_on_err
     
     try:
         u = URL(endpoint)
-        print(u)
         client = HTTPClient.from_url(u, connection_timeout=JSONRPC_API_REQUEST_TIMEOUT,
             network_timeout=JSONRPC_API_REQUEST_TIMEOUT)
         r = client.post(u.request_uri, body=json.dumps(payload), headers=headers)
