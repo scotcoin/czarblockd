7a5f377c797c88bd3c7d5dc5308f63afb8d0ea2f
Treefunder Incorporated <ceo@treefunder.com>
2014-10-25 06:23:24 -0400

    Czarblockd Factoring

--- lib/api.py
+++ lib/api.py
@@ -147,30 +147,30 @@ def serve_api(mongo_db, redis_client):
         for address in addresses:
             filters.append({'field': 'address', 'op': '==', 'value': address})
         
-        #mappings = {}
-        #result = util.call_jsonrpc_api("get_balances",
-        #    {'filters': filters, 'filterop': 'or'}, abort_on_error=True)['result']
-        results = []
-        offset = 0
-        limit = 1000
-        while True:
-            result = util.call_jsonrpc_api("get_balances",
-                {'filters': filters, 'filterop': 'or', 'offset': offset, 'limit': limit}, abort_on_error=True)['result']
-            results += result
-            if len(result) >= limit:
-                offset += len(result)
-            else:
-                break
+        mappings = {}
+        result = util.call_jsonrpc_api("get_balances",
+            {'filters': filters, 'filterop': 'or'}, abort_on_error=True)['result']
+        #results = []
+        #offset = 0
+        #limit = 1000
+        #while True:
+        #    result = util.call_jsonrpc_api("get_balances",
+        #        {'filters': filters, 'filterop': 'or', 'offset': offset, 'limit': limit}, abort_on_error=True)['result']
+        #    results += result
+        #    if len(result) >= limit:
+        #        offset += len(result)
+        #    else:
+        #        break
 
         isowner = {}
         owned_assets = mongo_db.tracked_assets.find( { '$or': [{'owner': a } for a in addresses] }, { '_history': 0, '_id': 0 } )
         for o in owned_assets:
           isowner[o['owner'] + o['asset']] = o
 
-        #data = []
-        #for d in result:
-        mappings = {}
-        for d in results:
+        data = []
+        for d in result:
+        #mappings = {}
+        #for d in results:
             if not d['quantity'] and ((d['address'] + d['asset']) not in isowner):
                 continue #don't include balances with a zero asset value
             asset_info = mongo_db.tracked_assets.find_one({'asset': d['asset']})
