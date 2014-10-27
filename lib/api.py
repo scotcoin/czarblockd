c87261ab631930920e883dc36dd6853e4a823f42
Treefunder Incorporated <ceo@treefunder.com>
2014-10-27 17:08:16 -0400

    API Checks

--- lib/api.py
+++ lib/api.py
@@ -65,9 +65,7 @@ def serve_api(mongo_db, redis_client):
         
     @dispatcher.add_method
     def get_asset_names():
-        #result = util.call_jsonrpc_api(method, params)
-        #return proxy_to_czarpartyd('get_asset_names', params=[])
-        result = util.call_jsonrpc_api("get_asset_names")
+        result = util.call_jsonrpc_api("get_asset_names")['result']
         print( result )
         return result['result']
     
